e07b7fd2b6503020758727fa48582ab0b0c0724f
Ali Farrag <afds1207@gmail.com>
2019-11-09 00:52:38 -0500

    Task: Created instantiation and result test function

--- src/CalculatorTest.py
+++ src/CalculatorTest.py
@@ -1,9 +1,14 @@
 import unittest
+from Calculator import Calculator
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_instantiate_calculator(self):
+        calculator = Calculator()
+        self.assertIsInstance(calculator, Calculator)
+    def test_results_properties(self):
+        calculator = Calculator()
+        self.assertEqual(calculator.result, 0)
 
 
 if __name__ == '__main__':
