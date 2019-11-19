83772171ec4bccd4e9ad44f055157d01d2f2227c
Ali Farrag <afds1207@gmail.com>
2019-11-19 13:21:32 -0500

    Feature: Created CSV reader class

--- src/CSVReader.py
+++ src/CSVReader.py
@@ -1,15 +1,37 @@
 import csv
+from pathlib import Path
 
-filename = "UnitTestAddition.csv"
-fields = []
-rows = []
-
-with open (filename, 'r') as csvfile:
-    csvreader = csv.reader(csvfile)
-    fields = csvreader.next()
-    for row in csvreader:
-        rows.append(row)
+from pprint import pprint
 
 
 
+def ClassFactory(class_name, dictionary):
+    return type(class_name, (object,), dictionary)
 
+
+class CsvReader:
+    data = []
+
+    def __index__(self, filepath):
+        with open(filepath) as text_data:
+            csv_data = csv.DictReader(text_data, delimiter=",")
+            for row in csv_data:
+                self.data.append(row)
+        pass
+
+    def __init__(self, filepath):
+        self.data = []
+        relative = Path(filepath)
+        absolute = relative.absolute()
+        with open(absolute) as text_data:
+            csv_data = csv.DictReader(text_data, delimiter=',')
+            for row in csv_data:
+                self.data.append(row)
+                pprint(row)
+        pass
+
+    def return_data_as_objects(self, class_name):
+        objects = []
+        for row in self.data:
+            objects.append(ClassFactory(class_name, row))
+        return objects
