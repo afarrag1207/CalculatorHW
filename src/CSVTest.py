168b95f72a9924712c69a2a90ee5f67eecbb99dc
Ali Farrag <afds1207@gmail.com>
2019-11-19 13:22:54 -0500

    Feature: Created CSV unittest class

--- src/CSVTest.py
+++ src/CSVTest.py
@@ -1,20 +1,18 @@
 import unittest
-from CSVReader  import CsvReader, ClassFactory
+from CSVReader import CsvReader, ClassFactory
 
 
 class MyTestCase(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.csv_reader = CsvReader('UnitTestAddition.csv')
+        self.csv_reader = CsvReader('/src/UnitTestAddition.csv')
 
     def test_return_data_as_objects(self):
-        Value1 = self.csv_reader.return_data_as_objects('Value1')
-        Value2 = self.csv_reader.return_data_as_objects('Value2')
-        Result = self.csv_reader.return_data_as_objects('Result')
-        self.assertIsInstance(Value1, Value2, Result, list)
-        test_class = ClassFactory('Value1', self.csv_reader.data[0])
-        for Value1 in Result:
-            self.assertEqual(Value1.__name__, test_class.__name__)
+        people = self.csv_reader.return_data_as_objects('person')
+        self.assertIsInstance(people, list)
+        test_class = ClassFactory('person', self.csv_reader.data[0])
+        for person in people:
+            self.assertEqual(person.__name__, test_class.__name__)
 
 
 if __name__ == '__main__':
